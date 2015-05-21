f01e7c9385e572c5847f05b9c5c33ca3d49041ad
taiti <taiti@stone-rise.com>
2015-05-21 18:36:29 +0900

    chapter33 - 1

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 // データベースへの接続
 
 try {
-	$dbh = new PDO('mysq:host=localhost;dbname=blog_app','root');
+	$dbh = new PDO('mysq:host=localhost;dbname=blog_app','dbuser','123456');
 } catch (PDOException $e) {
 	var_dump($e->getMessage());
 	exit;
