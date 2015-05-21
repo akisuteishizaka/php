d4b95fe597472bdb8ce0d946cdfcef29a4f61fb1
taiti <taiti@stone-rise.com>
2015-05-21 14:30:15 +0900

    chapter02

--- index.php
+++ index.php
@@ -1,3 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head lang="ja">
+	<meta charset="utf-8">
+	<meta name="discription" content="phpの練習">
+	<meta name="keywords" content="php">
+	<title>phpの練習</title>
+</head>
+<body>
 <?php
 	echo "hellow world";
 ?>
+</body>
+</html>
+
